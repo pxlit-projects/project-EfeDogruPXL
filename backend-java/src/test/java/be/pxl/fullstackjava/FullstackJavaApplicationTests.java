9891687ec80a67366e15ec07d9fedf78b3676881
Efe-Dogru <efedogru9@hotmail.com>
2024-11-13 10:34:58 +0100

    Architecture

---
+++ backend-java/src/test/java/be/pxl/fullstackjava/FullstackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.fullstackjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
